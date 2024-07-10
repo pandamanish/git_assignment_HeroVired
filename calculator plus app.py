38afc5e176c588a02e9af455fb1a03e670813caf
Manish Panda <pandamanish75@gmail.com>
2024-07-10 12:33:04 +0530

    bug fixed in the divide function

--- calculator plus app.py
+++ calculator plus app.py
@@ -16,8 +16,12 @@ class Calculator:
 
     def divide(self, a, b):
 
-        return a / b
+        if b == 0:
 
+            raise ValueError("Cannot divide by zero.")
+
+        return a / b
+    
     # TODO: Implement the following function to calculate the square root of a number.
 
     # def square_root(self, x):
